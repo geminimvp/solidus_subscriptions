04c484a982c0f6fdc0404e595776313b7aed06b7
Peter Berkenbosch <info@peterberkenbosch.nl>
2019-03-01 15:06:14 +0100

    count active subs that have a line_item

--- app/controllers/spree/admin/subscriptions_controller.rb
+++ app/controllers/spree/admin/subscriptions_controller.rb
@@ -75,7 +75,7 @@ module Spree
         this_month_range = Time.zone.now.beginning_of_month..Time.zone.now.end_of_month
         today_range = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
         tomorrow_range = Time.zone.tomorrow.beginning_of_day..Time.zone.tomorrow.end_of_day
-        active_subs = model_class.where(state: 'active')
+        active_subs = model_class.where(state: 'active').where('solidus_subscriptions_line_items.spree_line_item_id is not null')
 
         @total_active_subs = active_subs.count
         @total_active_subs_this_month = active_subs.where(actionable_date: this_month_range).count
