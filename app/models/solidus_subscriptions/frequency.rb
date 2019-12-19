015140e6660c1140a66bfc2bf4080a6ac29b601e
Luke Settle <lukewestonsettle@gmail.com>
2019-12-19 16:46:29 -0600

    Add prices to subscription frequencies

--- app/models/solidus_subscriptions/frequency.rb
+++ app/models/solidus_subscriptions/frequency.rb
@@ -1,6 +1,17 @@
 module SolidusSubscriptions
   class Frequency < ApplicationRecord
+
+    include Spree::DefaultPrice
+
     belongs_to :spree_variant, class_name: 'Spree::Variant', inverse_of: :frequencies
+
+    has_many(
+      :prices,
+      class_name: 'Spree::Price',
+      foreign_key: 'solidus_subscriptions_frequency_id',
+    )
+
+    accepts_nested_attributes_for :prices
 
     validates :length, :units, :spree_variant, presence: true
   end
