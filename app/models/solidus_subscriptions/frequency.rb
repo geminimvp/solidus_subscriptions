c9426fe96ec26edf25807ed930dc236ba9f2da47
Luke Settle <lukewestonsettle@gmail.com>
2019-12-20 10:57:37 -0600

    Fix frequency has_one default_price association

--- app/models/solidus_subscriptions/frequency.rb
+++ app/models/solidus_subscriptions/frequency.rb
@@ -3,6 +3,14 @@ module SolidusSubscriptions
 
     include Spree::DefaultPrice
 
+    has_one :default_price,
+      -> { with_deleted.currently_valid.with_default_attributes },
+      class_name: 'Spree::Price',
+      foreign_key: :solidus_subscriptions_frequency_id,
+      inverse_of: :frequency,
+      dependent: :destroy,
+      autosave: true
+
     belongs_to :spree_variant, class_name: 'Spree::Variant', inverse_of: :frequencies
 
     has_many(
